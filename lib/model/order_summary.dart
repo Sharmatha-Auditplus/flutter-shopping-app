79b47281011b5ea199f678bea252c000c9f43c1b
hchockarprasad <hchockarprasad@gmail.com>
2024-04-26 13:15:29 +0530

    Update

---
+++ lib/model/order_summary.dart
@@ -0,0 +1,9 @@
+import 'package:shopping_cart/pages/checkout_page.dart';
+
+OrderSummary globalOrderSummary = OrderSummary();
+
+class OrderSummary {
+  DeliveryMethod deliveryMethod = DeliveryMethod.pickup;
+  Map<String, String?>? deliveryAddress;
+// products
+}
--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_cart/model/order_summary.dart';
+import 'package:shopping_cart/pages/address_form.dart';
 import 'package:shopping_cart/pages/payment_page.dart';
 
 enum DeliveryMethod {
@@ -14,16 +16,8 @@ class CheckoutPage extends StatefulWidget {
 }
 
 class _CheckoutPageState extends State<CheckoutPage> {
-  DeliveryMethod? delivery = DeliveryMethod.pickup;
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String? country;
-  String? state;
-  String? fullName;
-  String? mobNumber;
-  String? pincode;
-  String? address;
-  String? area;
-  String? city;
+  DeliveryMethod delivery = DeliveryMethod.pickup;
+
   int selectedAddressIndex = -1;
   Color _textColor = Colors.green;
 
@@ -44,47 +38,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 20),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Radio<DeliveryMethod>(
-                      value: DeliveryMethod.pickup,
-                      groupValue: delivery,
-                      onChanged: (DeliveryMethod? value) {
-                        setState(() {
-                          delivery = value;
-                          if (value == DeliveryMethod.pickup) {
-                            _resetForm();
-                          } else {
-                            _textColor = Colors.black;
-                          }
-                        });
-                      },
-                    ),
-                    const Text('Store Pickup'),
-                    const SizedBox(width: 20),
-                    Radio<DeliveryMethod>(
-                      value: DeliveryMethod.homeDelivery,
-                      groupValue: delivery,
-                      onChanged: (DeliveryMethod? value) {
-                        setState(() {
-                          delivery = value;
-                          if (value == DeliveryMethod.homeDelivery) {
-                            _textColor = Colors.black;
-                            _showAddressForm();
-                          } else {
-                            _resetForm();
-                          }
-                        });
-                      },
-                    ),
-                    const Text('Home Delivery'),
-                  ],
-                ),
-              ),
-            ),
+            _buildModeSelection(),
             const SizedBox(height: 10),
             if (delivery == DeliveryMethod.pickup)
               TextButton(
@@ -100,37 +54,37 @@ class _CheckoutPageState extends State<CheckoutPage> {
                   style: TextStyle(color: Colors.green, decoration: TextDecoration.underline),
                 ),
               ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  setState(() {
-                    _textColor = Colors.black;
-                    _showAddressForm();
-                  });
-                },
-                //hoverColor: Colors.transparent,
-                child: Row(
-                  children: [
-                    const Icon(
-                      Icons.add,
-                      color: Colors.grey,
-                    ),
-                    const SizedBox(width: 5),
-                    Text(
-                      'Add New Address',
-                      style: TextStyle(
-                        color: _textColor,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                        decoration: delivery == DeliveryMethod.homeDelivery ? TextDecoration.underline : null,
+            if (delivery == DeliveryMethod.homeDelivery) ...[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                  onTap: () {
+                    setState(() {
+                      _textColor = Colors.black;
+                      _showAddressForm();
+                    });
+                  },
+                  //hoverColor: Colors.transparent,
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.add,
+                        color: Colors.grey,
                       ),
-                    ),
-                  ],
+                      const SizedBox(width: 5),
+                      Text(
+                        'Add New Address',
+                        style: TextStyle(
+                          color: _textColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          decoration: delivery == DeliveryMethod.homeDelivery ? TextDecoration.underline : null,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            if (addresses.isNotEmpty && delivery == DeliveryMethod.homeDelivery) ...[
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.6,
                 width: MediaQuery.of(context).size.width * 0.9,
@@ -160,8 +114,8 @@ class _CheckoutPageState extends State<CheckoutPage> {
                             '${addresses[index]['city']},${addresses[index]['state']},${addresses[index]['pincode']}',
                             style: const TextStyle(color: Colors.grey, fontWeight: FontWeight.normal, fontSize: 14),
                           ),
-                          Text(
-                            addresses[index]['country']!,
+                          const Text(
+                            'INDIA',
                             style: const TextStyle(color: Colors.grey, fontWeight: FontWeight.normal, fontSize: 14),
                           ),
                           Text(
@@ -186,9 +140,11 @@ class _CheckoutPageState extends State<CheckoutPage> {
             children: [
               const Spacer(),
               if (delivery == DeliveryMethod.pickup ||
-                  (delivery == DeliveryMethod.homeDelivery && _isAddressFormFilled()))
+                  (delivery == DeliveryMethod.homeDelivery && selectedAddressIndex != -1))
                 TextButton(
                   onPressed: () {
+                    globalOrderSummary.deliveryMethod = delivery;
+                    globalOrderSummary.deliveryAddress = addresses[selectedAddressIndex];
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => const PaymentPage()),
@@ -210,199 +166,58 @@ class _CheckoutPageState extends State<CheckoutPage> {
     );
   }
 
-  void _showAddressForm() {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.only(
-              bottom: MediaQuery.of(context).viewInsets.bottom,
+  Widget _buildModeSelection() {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Radio<DeliveryMethod>(
+              value: DeliveryMethod.pickup,
+              groupValue: delivery,
+              onChanged: (DeliveryMethod? value) {
+                setState(() {
+                  if (value != null) {
+                    delivery = value;
+                  }
+                });
+              },
             ),
-            child: Form(
-              key: _formKey,
-              child: Padding(
-                padding: const EdgeInsets.all(12),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Fill Delivery Address:',
-                      style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold, fontSize: 16),
-                    ),
-                    DropdownButtonFormField(
-                      value: country,
-                      items: const [
-                        DropdownMenuItem(value: 'India', child: Text('India')),
-                        DropdownMenuItem(value: 'Japan', child: Text('Japan')),
-                        DropdownMenuItem(value: 'China', child: Text('China')),
-                        DropdownMenuItem(value: 'France', child: Text('France')),
-                        DropdownMenuItem(value: 'Ireland', child: Text('Ireland')),
-                      ],
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          country = newValue;
-                        });
-                      },
-                      decoration: const InputDecoration(labelText: 'Country'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please select a country';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      onChanged: (value) => fullName = value,
-                      decoration: const InputDecoration(labelText: 'Full Name'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your full name';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      onChanged: (value) => mobNumber = value,
-                      decoration: const InputDecoration(labelText: 'Mob Number'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your mobile number';
-                        }
-                        if (value.length != 10) {
-                          return 'Mobile number must be 10 digits';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      onChanged: (value) {
-                        pincode = value;
-                      },
-                      decoration: const InputDecoration(labelText: 'Pincode'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter a pincode';
-                        }
-                        if (value.length != 6) {
-                          return 'Pincode must be 6 digits';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      onChanged: (value) => address = value,
-                      decoration: const InputDecoration(labelText: 'Flat,House.no,Company,Apartment'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your address';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      onChanged: (value) => area = value,
-                      decoration: const InputDecoration(labelText: 'Area,Street,Sector'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your area';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(labelText: 'Landmark'),
-                    ),
-                    TextFormField(
-                      onChanged: (value) => city = value,
-                      decoration: const InputDecoration(labelText: 'Town,City'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your city';
-                        }
-                        return null;
-                      },
-                    ),
-                    DropdownButtonFormField(
-                      value: state,
-                      items: const [
-                        DropdownMenuItem(value: 'TamilNadu', child: Text('TamilNadu')),
-                        DropdownMenuItem(value: 'Kerala', child: Text('Kerala')),
-                        DropdownMenuItem(value: 'Karnataka', child: Text('Karnataka')),
-                        DropdownMenuItem(value: 'Punjab', child: Text('Punjab')),
-                        DropdownMenuItem(value: 'Gujarat', child: Text('Gujarat')),
-                        DropdownMenuItem(value: 'Madhya Pradesh', child: Text('Madhya Pradesh')),
-                      ],
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          state = newValue;
-                        });
-                      },
-                      decoration: const InputDecoration(labelText: 'State'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please select a state';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 8),
-                    Center(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            addresses.add({
-                              'country': country,
-                              'state': state,
-                              'fullName': fullName,
-                              'mobNumber': mobNumber,
-                              'pincode': pincode,
-                              'address': address,
-                              'area': area,
-                              'city': city,
-                            });
-                            setState(() {
-                              selectedAddressIndex = addresses.length - 1;
-                              _resetForm();
-                            });
-                            Navigator.pop(context);
-                          }
-                        },
-                        child: const Text('Use this Address'),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            const Text('Store Pickup'),
+            const SizedBox(width: 20),
+            Radio<DeliveryMethod>(
+              value: DeliveryMethod.homeDelivery,
+              groupValue: delivery,
+              onChanged: (DeliveryMethod? value) {
+                setState(() {
+                  if (value != null) {
+                    delivery = value;
+                  }
+                });
+              },
             ),
-          ),
-        );
-      },
+            const Text('Home Delivery'),
+          ],
+        ),
+      ),
     );
   }
 
-  bool _isAddressFormFilled() {
-    return country != null &&
-        state != null &&
-        fullName != null &&
-        mobNumber != null &&
-        pincode != null &&
-        address != null &&
-        area != null &&
-        city != null;
-  }
-
-  void _resetForm() {
-    setState(() {
-      country = null;
-      state = null;
-      fullName = null;
-      mobNumber = null;
-      pincode = null;
-      address = null;
-      area = null;
-      city = null;
-      _formKey.currentState?.reset();
+  void _showAddressForm() {
+    showModalBottomSheet<Map<String, String?>>(
+      isScrollControlled: true,
+      context: context,
+      useSafeArea: true,
+      builder: (BuildContext context) {
+        return const AddressForm();
+      },
+    ).then((value) {
+      if (value != null) {
+        setState(() {
+          addresses.add(value);
+          selectedAddressIndex = addresses.length - 1;
+        });
+      }
     });
   }
 }
